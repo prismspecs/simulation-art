fb51fb0e5a0dd433cdd5fe0f3bd9d82d07b9b323
labuser <labuser@ip-80-161.wireless.oberlin.edu>
2018-10-11 16:03:37 -0400

    ...

---
+++ unity-workbench/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // what shall I follow?
+    public Transform Target;
+    // since doing transform.position = Target.position
+    // will place the camera inside the third person,
+    // we need to offset our view
+    public Vector3 Offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // set the camera pos equal to the target pos, and offset it
+        transform.position = Target.position + Offset;
+	}
+}
