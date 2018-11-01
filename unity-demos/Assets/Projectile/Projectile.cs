2b1a959183c71d721fab6093b3c2cd292a5299a7
labuser <labuser@ip-80-161.wireless.oberlin.edu>
2018-11-01 14:41:57 -0400

    gui demo

--- unity-demos/Assets/Projectile/Projectile.cs
+++ unity-demos/Assets/Projectile/Projectile.cs
@@ -8,9 +8,14 @@ public class Projectile : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        // if left mouse button is pressed
         if (Input.GetMouseButtonDown(0)) {
+            // spawn an object but also save a reference to that object
+            // as a GameObject type and name it 'go'
             GameObject go = Instantiate(projectile, transform.position, Quaternion.identity);
 
+            // tell go GameObject to addforce to rigidbody, relative to
+            // player's transform.forward
             go.GetComponent<Rigidbody>().GetComponent<Rigidbody>().AddForce(transform.forward * 500);
         }
 
