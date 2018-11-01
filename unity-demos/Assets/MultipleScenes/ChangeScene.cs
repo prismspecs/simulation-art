6bfcb84a06d0d961e1c71e4b2128c5e9b85d4fba
Grayson Earle <gearle@ip-80-103.wireless.oberlin.edu>
2018-11-01 13:25:45 -0400

    added demos

---
+++ unity-demos/Assets/MultipleScenes/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// NOTICE we have to use some additional functionality
+using UnityEngine.SceneManagement;
+
+// ALSO you must add both scene to the "build settings"
+// you do this by going to File->Build Settings
+// and then drag the relevant scenes into "Scenes in Build"
+
+
+public class ChangeScene : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        // if you are going to reference the scene by name
+        // make sure you are precise!
+        SceneManager.LoadScene("Scene2");
+    }
+}
---
+++ unity-demos/Assets/Projectile/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public GameObject projectile;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject go = Instantiate(projectile, transform.position, Quaternion.identity);
+
+            go.GetComponent<Rigidbody>().GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+        }
+
+    }
+}
